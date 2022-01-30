09083531d7d5fd05fb5163a86da0523e0827825f
Kickko <98144689+Kickko@users.noreply.github.com>
2022-01-30 17:25:52 +0900

    JS.Navbar menu click

--- main.js
+++ main.js
@@ -5,10 +5,23 @@ const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 window.addEventListener("scroll", () => {
-  console.log(window.scrollY);
+  // console.log(window.scrollY);
   if (window.scrollY >= navbarHeight) {
     document.body.classList.add("fixed-nav");
   } else {
     document.body.classList.remove("fixed-nav");
   }
+});
+
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
 });
